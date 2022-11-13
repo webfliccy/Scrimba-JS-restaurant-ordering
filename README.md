4d5edbff6b8ee907ce357a1fe814a4000e7b9c42
webfliccy <f.elicity.anderson@gmail.com>
2022-11-13 17:38:39 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# cobde4af6b5e0aaca4bf11acf
+
+## Solo project - Restaurant Ordering App
+
+Essential JavaScript Concepts (Part of Scrimba's Frontend Development career path)
+
+Concepts covered:
+- forEach()
+- data attributes
+- conditionally render styles
+- NOT operator (!)
+- Array.filter()
+- includes()
+- querySelectorAll()
+- addEventListener()
+- event.target
+
+- [The Frontend Career Path](https://scrimba.com/learn/frontend)
+- [Solo Project - restaurant ordering app](https://scrimba.com/learn/frontend/solo-project-restaurant-ordering-app-co72e499baf5f48346e5975cf)
+Happy Coding!
---
+++ index.js
@@ -0,0 +1,117 @@
+/* 
+=== Requirements ===
+
+[x] Replicate design: 
+    https://www.figma.com/file/Hdgwo69Dym9vVsxbuPbl0h/Mobile-Restaurant-Menu?node-id=0%3A1
+[x] Render menu options using JS
+[x] Add / remove items
+[x] Payment modal with compulsory form inputs
+
+=== Stretch goals ===
+
+* Change theme
+* Offer a discount
+* Allow users to rate experience
+
+*/
+
+import {menuArray} from './data.js'
+
+
+let orderArray = []
+
+document.addEventListener("click", function(e){
+    if (e.target.classList.contains('menu-item__btn')) {    
+        addToOrder(e.target.dataset.itemname, e.target.dataset.itemprice, e.target.dataset.itemid)
+    } 
+    else if (e.target.classList.contains('button-link')){
+        removeItem(e.target.dataset.itemid)
+    } 
+    else if (e.target.id === "cc-modal" || e.target.id === "submit-order" || e.target.id === "cc-modal-close"){
+        document.getElementById("cc-modal").classList.toggle("hidden")
+    }
+    else if (e.target.id === "submit-payment"){
+        e.preventDefault()
+        document.getElementById("cc-modal").classList.toggle("hidden")
+        //disable order buttons
+        const buttons = document.querySelectorAll(".menu-item__btn")
+        buttons.forEach(function(item){item.disabled = true})
+        //show success msg
+        const customerName = document.getElementById("customer-name")
+        document.getElementById("checkout").innerHTML = `<div class="success-msg">Thanks, ${customerName.value}! Your order is on its way!</div>`
+    }
+    
+})
+
+function renderPage() {
+    const menuContainer = document.getElementById('menu')
+    menuArray.forEach(function(item){
+    menuContainer.innerHTML += `
+    <article class="menu-item">
+                <div class="menu-item__details">
+                    <span class="menu-item__emoji">${item.emoji}</span>
+                    <div>
+                        <h2 class="menu-item__title">${item.name}</h2>
+                        <p class="menu-item__ingredients">${item.ingredients}</p>
+                        <p class="menu-item__price">$${item.price}</p>
+                    </div>
+                </div>
+                <button class="menu-item__btn" title="Add to order" data-itemid="${item.id}" data-itemprice="${item.price}" data-itemname="${item.name}">+</button>
+            </article>`
+    })
+}
+
+
+function addToOrder(name, price, id){
+    //check if item already in order
+    const matchingItem = orderArray.filter(function(item){
+            return item.id.includes(id)
+    })[0]
+    if (matchingItem) { 
+        const newPrice = Number(matchingItem.price) + Number(price)
+        matchingItem.price = newPrice
+        matchingItem.quantity += 1
+    } else {
+         orderArray.push({
+            "name": name,
+            "price": price,
+            "id": id,
+            "quantity": 1,
+        })  
+    }
+    renderOrder()
+}
+
+function renderOrder() {
+    const checkoutArea = document.getElementById('checkout-list')
+    const checkoutPrice = document.getElementById('checkout-price')
+    let checkoutTotal = 0
+    let checkoutList
+    if (orderArray.length > 0) {
+        checkoutArea.innerHTML = `<ul class="checkout-list"></ul>`
+        checkoutList = document.querySelector("ul.checkout-list")
+    } else {
+        checkoutArea.innerHTML = ''
+    }
+    orderArray.forEach(function(item){
+        checkoutList.innerHTML += `
+        <li class="checkout-item">
+            <span>${item.name} (${item.quantity})
+                <button class="button-link" data-itemid="${item.id}">remove</button>
+            </span>
+            <span class="checkout-price">$${item.price} </span>
+        </li>   
+      `
+        checkoutTotal += Number(item.price)
+    })
+    checkoutPrice.textContent = "$" + checkoutTotal
+}
+
+function removeItem(itemid) {
+    orderArray = orderArray.filter(function(item){
+        return !item.id.includes(itemid)
+    })
+    renderOrder()
+}
+
+renderPage()
